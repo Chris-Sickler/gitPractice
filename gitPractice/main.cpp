2a24e4bc591b4fe1e73bfc5c3dac9897ba4c95b5
Chris Sickler <sicklerc4@students.rowan.edu>
2020-01-25 18:22:24 -0500

    Second commit

--- gitPractice/main.cpp
+++ gitPractice/main.cpp
@@ -5,5 +5,7 @@ using namespace std;
 int main()
 {
     cout << "Hello world!" << endl;
+    cout << "Added after init commit" << endl;
+    cout << "Added after 2nd comment, first change currently staged but not committed" << endl;
     return 0;
 }
